2291ebda013e362dd3274dace71ca80c4b590004
Jose Wendel <wendelpviana@gmail.com>
2022-11-21 21:26:01 -0300

    initial commit

--- main.js
+++ main.js
@@ -23,20 +23,21 @@ function createCard(date, day, games){
 }
 
 document.querySelector("#cards").innerHTML =
-     createCard(
-        "24/11",
-        "quinta",
-        createGame("brazil", "16:00", "servia") +
-        createGame("brazil", "16:00", "servia")
-     ) +
-    createCard(
-        "28/11", 
-        "segunda", 
-        createGame("brazil", "16:00", "switzerland")
-    ) +
-    createCard(
-        "28/11", 
-        "segunda", 
-        createGame("brazil", "16:00", "cameroon")
-    )
+  createCard("20/11", "Domingo", createGame("qatar", "13:00", "ecuador")) +
+  createCard(
+    "21/11",
+    "Segunda",
+    createGame("england", "10:00", "iran") +
+      createGame("senegal", "13:00", "netherlands") +
+      createGame("united-states", "16:00", "wales")
+  ) +
+  createCard(
+    "28/11",
+    "Terça",
+    createGame("argentina", "07:00", "saudi-arabia") +
+    createGame("denmark", "10:00", "tunisia") +
+    createGame("mexico", "13:00", "poland") +
+    createGame("france", "16:00", "australia")
+  )
+ 
 
